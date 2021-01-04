b0954134634e4e3c833c05ebcdc5d41aa94f7e7d
mecomonteshbtn <1574@holbertonschool.com>
2021-01-04 08:36:57 -0500

    Minimum operations file

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -2,19 +2,13 @@
 
 /**
  * heap_insert - Inserts a value into a Max Binary Heap
- *
  * @root: Double pointer to the root node of the Heap
- *
  * @value: The value store in the node to be inserted
- *
  * Return: A tree with max heap ordering
  */
-
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *node, *parent;
-
-	/* For an empty tree, create nodes and return tree pointer to tree */
 	if (*root == NULL)
 	{
 		node = binary_tree_node(*root, value);
@@ -22,23 +16,17 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (*root);
 	}
 	node = *root;
-
-	/* Traverse tree */
 	while (node != NULL)
 	{
 		parent = node;
 		/* Ignore duplicates */
 		if (node->n == value)
 			return (node_swap(node));
-
-		/* Sort tree into BST formation */
 		if (value > node->n)
 			node = parent->left;
 		else
 			node = node->right;
 	}
-
-	/* Sort parent nodes */
 	if (value > parent->n)
 	{
 		parent->left = binary_tree_node(parent, value);
@@ -54,12 +42,9 @@ heap_t *heap_insert(heap_t **root, int value)
 
 /**
  * node_swap - Swaps current node with larger node
- *
  * @node: Pointer to current node
- *
  * Return: A new node
  */
-
 heap_t *node_swap(heap_t *node)
 {
 	while (node && node->parent)
