d8f8c9851d5e94a3e536df7860860be152eac787
mecomonteshbtn <1574@holbertonschool.com>
2021-01-28 15:57:49 -0500

    Write a script that reads stdin line by line and computes metrics

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,6 +7,7 @@ Created on Thu Jan 28 15:54:21 2021
 """
 import sys
 
+
 file_size = 0
 status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
@@ -21,11 +22,11 @@ try:
         file_size += eval(splited[-1])
         if i % 10 == 0:
             print("File size: {}".format(file_size))
-            for key,value in status_codes.items():
+            for key, value in status_codes.items():
                 if value > 0:
                     print("{}: {}".format(key, value))
 finally:
     print("File size: {}".format(file_size))
-    for key,value in status_codes.items():
+    for key, value in status_codes.items():
         if value > 0:
             print("{}: {}".format(key, value))
